887491a0d95a95b0c56bc6bc717b9fc542b10f5c
garimanexzem <garima@nexzem.com>
2025-06-23 16:42:22 +0530

    new repo

---
+++ JavaFiles/objects.js
@@ -0,0 +1,10 @@
+const JsUser = {
+    name: "Garima Negi",
+    age: 20,
+    location: "Dehradun",
+    email: "garima@google.com",
+    isLoggedIn: false,
+    lastLoginDays: ["Monday", "Saturday"]
+}
+
+console.log(JsUser.location)
