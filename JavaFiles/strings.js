80d628c98189f5aa394ab0381a44135184809ce4
garimanexzem <garima@nexzem.com>
2025-06-23 13:04:43 +0530

    new repo

---
+++ JavaFiles/strings.js
@@ -0,0 +1,6 @@
+const name ="Garima"
+const repoCount = 100
+
+//console.log(name + repoCount + "Value"); bad practice for concatenation
+
+console.log(`Hello my name is ${name} and my repo count is ${repoCount}`); // concatenation
